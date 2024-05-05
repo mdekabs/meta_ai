71fbe4cdadf6257c1d7ca8488a8130d5f5a35af8
mdekabs <1mawoda@gmail.com>
2024-05-05 03:15:41 +0100

    done

---
+++ meta.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from meta_ai_api import MetaAI
+
+app = Flask(__name__)
+
+class MetaAIService:
+    def __init__(self):
+        self.ai = MetaAI()
+
+    def generate_response(self, message):
+        return self.ai.prompt(message=message)
+
+ai_service = MetaAIService()
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    message = data['message']
+    response = ai_service.generate_response(message)
+    return jsonify({'response': response})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
